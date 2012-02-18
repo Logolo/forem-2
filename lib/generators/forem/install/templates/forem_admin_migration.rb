c52f26047122c594d6b9645cab517668de406cdc
Juan Felipe Rincón <ifs@saddlesores.org>
2012-02-18 12:55:37 -0500

    Remvoed AddForemAdmin migration change call, assuming that in Mongoid user model will have a :forem_admin flag

--- lib/generators/forem/install/templates/forem_admin_migration.rb
+++ lib/generators/forem/install/templates/forem_admin_migration.rb
@@ -1,10 +1,6 @@
 class AddForemAdmin < ActiveRecord::Migration
 
   def change
-<<<<<<< HEAD
 #    add_column <%= user_class.constantize.table_name.to_sym.inspect %>, :forem_admin, :boolean, :default => false
-=======
-    add_column <%= user_class.constantize.table_name.to_sym.inspect %>, :forem_admin, :boolean, :default => false
->>>>>>> 5e3a14171f1940f9bbefd74f58ec546ca692d263
   end
 end
