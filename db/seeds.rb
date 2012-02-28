bacc776826e677d4d958688bde03eaa3dd9ef3fb
Juan Felipe Rincón <ifs@saddlesores.org>
2012-02-28 08:42:14 -0500

    Seeds for category in forem seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
 user = Forem.user_class.first
 unless user.nil?
+  category = Forem::Forum.find_or_create_by(:name => "Group")
+  category2 = Forem::Forum.find_or_create_by(:name => "Open Forum")
+  
   forum = Forem::Forum.find_or_create_by( :category_id => Forem::Category.first.id, 
                                :title => "Default",
                                :description => "Default forem created by install")
