ab467a6563ec6066972baf8250b68a7fb1712036
MonsieurApple <tony@bzextreme.com>
2012-06-14 17:52:35 +0900

    Change members javascript to work with bootstrap typeahead

--- app/assets/javascripts/forem/admin/members.js
+++ app/assets/javascripts/forem/admin/members.js
@@ -4,22 +4,15 @@
 //= require forem
 //
 $(document).ready(function() {
-  group_id = $('#add_member').data('group-id');
-  $('#new_member').autocomplete({source: Forem.base_path + '/admin/users/autocomplete'})
-
-  $("#new_member").bind("autocompleteselect", function(event, ui) {
-    $("#add_member").attr('disabled', false)
-  })
+  group_id = $('#new_member').data('group-id');
 
   add_member = function() {
     user = $("#new_member").val()
     $.post(Forem.base_path + '/admin/groups/' + group_id + '/members', { user: user })
-    $(this).attr('disabled', true)
     $("#new_member").val("")
     $('#members').append('<li>' + user + '</li>')
   }
 
-  $('#add_member').click(add_member)
   $('#new_member').keypress(function(e){
     if (e.which == 13) {
       add_member();
