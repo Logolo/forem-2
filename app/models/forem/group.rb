9382c23f9e56402a66c351e43ca99473ca7023dd
mrapple <tony@bzextreme.com>
2012-06-16 20:14:57 +0900

    Make badge_color accessible

--- app/models/forem/group.rb
+++ app/models/forem/group.rb
@@ -12,7 +12,7 @@ module Forem
 
     has_many :members, :class_name => Forem.user_class.to_s
 
-    attr_accessible :name, :html_color, :mc_color, :mc_permissions
+    attr_accessible :name, :html_color, :badge_color, :mc_color, :mc_permissions
 
     def to_s
       name
