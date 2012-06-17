7dd78ef3d190b5d6cefbe4e16dae4353282bb2aa
mrapple <tony@bzextreme.com>
2012-06-17 18:36:36 +0900

    Add priority + priority sorting, fix relations

--- app/models/forem/group.rb
+++ app/models/forem/group.rb
@@ -7,12 +7,19 @@ module Forem
     field :badge_color, :default => "none"
     field :mc_color
     field :mc_permissions
+    field :priority, :type => Integer, :default => 0
 
     validates :name, :presence => true
 
-    has_many :members, :class_name => Forem.user_class.to_s
+    has_and_belongs_to_many :members, :class_name => Forem.user_class.to_s, :inverse_of => :groups
 
     attr_accessible :name, :html_color, :badge_color, :mc_color, :mc_permissions
+
+    class << self
+        def by_priority
+            order_by(:priority)
+        end
+    end
 
     def to_s
       name
