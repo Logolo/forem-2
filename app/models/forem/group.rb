88b647dc26af500d1ca34dcf6755aee886117695
mrapple <tony@bzextreme.com>
2012-07-06 23:12:48 -0500

    Groups have some permissions now

--- app/models/forem/group.rb
+++ app/models/forem/group.rb
@@ -8,12 +8,15 @@ module Forem
     field :mc_color
     field :mc_permissions
     field :priority, :type => Integer, :default => 0
+    field :can_view, :type => Boolean, :default => false
+    field :can_create, :type => Boolean, :default => false
+    field :can_reply, :type => Boolean, :default => false
 
     validates :name, :presence => true
 
     has_and_belongs_to_many :members, :class_name => Forem.user_class.to_s, :inverse_of => :groups
 
-    attr_accessible :name, :html_color, :badge_color, :mc_color, :mc_permissions, :priority
+    attr_accessible :name, :html_color, :badge_color, :mc_color, :mc_permissions, :priority, :can_view, :can_create, :can_reply
 
     class << self
         def by_priority
