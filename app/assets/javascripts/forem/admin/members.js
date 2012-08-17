dcfe646023f48e1091b4c2f9962dfc3e8d1d78c8
mrapple <tony@oc.tc>
2012-08-16 19:11:50 -0500

    Update javascript to comply with new *puke* routes

--- app/assets/javascripts/forem/admin/members.js
+++ app/assets/javascripts/forem/admin/members.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
   add_member = function() {
     user = $("#new_member").val()
-    $.post(Forem.base_path + '/admin/groups/' + group_id + '/members', { user: user })
+    $.post(Forem.base_path + '/admin/forums/groups/' + group_id + '/members', { user: user })
     $("#new_member").val("")
     $('#members').append('<li>' + user + '</li>')
   }
