60e60ef87b32df51129a24ac3afeef8aafd6d7fc
mrapple <tony@oc.tc>
2012-11-05 21:47:13 -0600

    Move online users code into the controllers

--- app/controllers/forem/application_controller.rb
+++ app/controllers/forem/application_controller.rb
@@ -2,12 +2,33 @@ require 'cancan'
 
 class Forem::ApplicationController < ApplicationController
 
+  before_filter :calculate_online_users
+
   rescue_from CanCan::AccessDenied do
     redirect_to root_path, :alert => t("forem.access_denied")
   end
 
   def current_ability
     Forem::Ability.new(forem_user)
+  end
+
+  def calculate_online_users
+    users = Forem.user_class.where(:last_page_load.gt => 10.minutes.ago).limit(40)
+    max = 0
+    @online_list = {}
+
+    users.each do |u|
+        group = Forem::Group.where(:members => u.username).asc(:priority).first
+        if group == nil
+            max = (2**(0.size * 8 -2) -1) # fixnum max
+        else
+            max = group.priority
+        end
+
+        @online_list[max] = Hash.new if @online_list[max] == nil
+
+        @online_list[max].merge!({u.username => (group == nil || group.html_color == nil ? "" : group.html_color)})
+    end
   end
 
   private
@@ -19,6 +40,7 @@ class Forem::ApplicationController < ApplicationController
       redirect_to Forem.sign_in_path || main_app.sign_in_path
     end
   end
+
 
   def forem_admin?
     forem_user && forem_user.forem_admin?
